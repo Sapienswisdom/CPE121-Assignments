79bf1ba43a4fea0f0efc2b59a34ca15a1e56c019
Sapienswisdom <sapienswisdom2@gmail.com>
2026-07-02 13:41:22 +0100

    Add files via upload
    This is the file for number 1 of the assignment.

---
+++ CPE Assignment 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<< "CHIBUIKE, WISDOM CHIEMERIE" <<endl;
+	cout<< "25/EG/CO/013"<<endl;
+	cout<< "COMPUTER ENGINEERING"<<endl;
+	
+	return 0;
+}
