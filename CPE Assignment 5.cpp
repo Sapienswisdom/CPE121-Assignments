85d2995b74a59398e5f4de38931008b746819cb6
Sapienswisdom <sapienswisdom2@gmail.com>
2026-07-02 14:32:47 +0100

    Add files via upload
    This is the file for the CPE121 Assignment question 2.

---
+++ CPE Assignment 5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Rain, rain go away"<<endl;
+	cout<<"Come again another day"<<endl;
+	cout<<"Little Johnny wants to play"<<endl;
+	cout<<"Rain,rain go away"<<endl;
+	return 0;
+}
