a982ee6fb58be24e718ce43aecb1b1ce2a8841ca
Sapienswisdom <sapienswisdom2@gmail.com>
2026-07-02 14:24:09 +0100

    Add files via upload
    This is the file for the \CPE121 Assignment, question 4.

---
+++ CPE Assignment 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//This cout statememt prints outs my friends name is Raphael.
+	cout<<"My friend's name is Raphael"<<	endl;
+	
+	//This cout statement prints out his favorite car is Toyota
+	cout<<"His car is Toyota"<<endl;
+	
+	//This cout statement prints out he is in level 100
+	cout<<"He is in Level 100"<<endl;
+	return 0;
+;}
